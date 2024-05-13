7e8b265bc9a98a8cb5cc05d135725b2930432eea
d.zhiborkin <danilkaololo67@gmail.com>
2024-05-13 22:59:45 +0300

    Update message.js

--- src/js/message.js
+++ src/js/message.js
@@ -1,3 +1,5 @@
-export function displayValidationMessage(element, message) {
+// eslint-disable-next-line import/prefer-default-export
+export default function displayValidationMessage (element, message) {
+    // eslint-disable-next-line no-param-reassign
     element.textContent = message;
 }
